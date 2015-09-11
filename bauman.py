45898c1cc459ff1675dab5fdbc08fcd6897e1e96
igorcoding <igorcoding@gmail.com>
2015-09-11 16:15:27 +0300

    Improved timetable structure for bmstu

--- bauman.py
+++ bauman.py
@@ -3,26 +3,58 @@
 
 import datetime
 from datetime import datetime as d
+from pprint import pprint
 import unicodecsv
+import re
 
 from tp import GCalendarEntry
 
 
+LESSONS = [
+    {'begin': '08:30:00 AM', 'end': '10:05:00 AM'},
+    {'begin': '10:15:00 AM', 'end': '11:50:00 AM'},
+    {'begin': '12:00:00 PM', 'end': '01:35:00 PM'},
+    {'begin': '01:50:00 PM', 'end': '03:25:00 PM'},
+    {'begin': '03:40:00 PM', 'end': '05:15:00 PM'},
+    {'begin': '05:25:00 PM', 'end': '07:00:00 PM'},
+    {'begin': '07:10:00 PM', 'end': '08:45:00 PM'},
+]
+
 class TimeTableEntry:
+    @staticmethod
+    def str_to_time(s):
+        return d.strptime(s, "%I:%M:%S %p")
+
     def __init__(self, data):
         self.day = data['day']
         self.nomdom = data['nomdom']
         self.discipline = data['discipline']
         self.location = data['location']
         self.type = data['type']
-        self.begin_time = d.strptime(data['begin'], "%I:%M:%S %p")
-        self.end_time = d.strptime(data['end'], "%I:%M:%S %p")
+
+        self.lessons = data['lessons']
+        m = re.match(r'(.+)-(.+)', self.lessons)
+        if m is None:
+            m = re.match('\s*(\d+)\s*', self.lessons)
+            if m is None or m.group(1) is None:
+                raise Exception("Wrong lessons format: %s" % self.lessons)
+            else:
+                first_lesson = LESSONS[int(m.group(1)) - 1]
+                self.begin_time = self.str_to_time(first_lesson['begin'])
+                self.end_time = self.str_to_time(first_lesson['end'])
+        else:
+            m2 = re.match('\s*(\d+)\s*-\s*(\d+)\s*', self.lessons)
+            if m2 is None or m2.group(1) is None or m2.group(2) is None:
+                if m.group(1) is None or m.group(2) is None:
+                    raise Exception("Wrong lessons format: %s" % self.lessons)
+                self.begin_time = self.str_to_time(m.group(1))
+                self.end_time = self.str_to_time(m.group(2))
+            else:
+                self.begin_time = self.str_to_time(LESSONS[int(m2.group(1)) - 1]['begin'])
+                self.end_time = self.str_to_time(LESSONS[int(m2.group(2)) - 1]['end'])
 
 
 class BaumanParser:
-    first_day = d.strptime("02.02.2015", "%d.%m.%Y")
-    last_day = d.strptime("30.04.2015", "%d.%m.%Y")
-
     weekdays = {
         'mon': 0,
         'tue': 1,
@@ -42,12 +74,12 @@ class BaumanParser:
 
     nomdom = ['nom', 'dom']
 
-    def __init__(self):
-        pass
+    def __init__(self, first_day, last_day):
+        self.first_day = d.strptime(first_day, "%d.%m.%Y")
+        self.last_day = d.strptime(last_day, "%d.%m.%Y")
 
     def parse(self, input_file, output_file):
         timetable = list(self._read_file(input_file))
-
 
         current_day = self.first_day
 
@@ -101,6 +133,6 @@ class BaumanParser:
 
 
 if __name__ == "__main__":
-    parser = BaumanParser()
+    parser = BaumanParser("01.09.2015", "31.12.2015")
     parser.parse("bauman.csv", "out_bauman.csv")
-    print 'hi'
+    print 'ok'
